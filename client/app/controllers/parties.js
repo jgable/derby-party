3cee5b9d32fac15e87efe47537ea449aa163daf6
Jacob Gable <jacob.gable@gmail.com>
2014-04-30 19:03:37 -0500

    Merging with changes from Ryan

--- client/app/controllers/parties.js
+++ client/app/controllers/parties.js
@@ -4,18 +4,13 @@ export default Ember.ArrayController.extend(LoggedInMixin, {
     partyName: null,
     error: null,
 
-    currentParties: function () {
-        return this.get('content').toArray();
-    }.property('content', 'content.isLoaded', 'content.isUpdating'),
-
     isEmpty: function () {
-        return this.get('currentParties').length < 1;
-    }.property('currentParties'),
+        return this.content.get('length') < 1;
+    }.property('content.length'),
 
     actions: {
         makeParty: function () {
-            var self = this,
-                partyName = this.get('partyName');
+            var partyName = this.get('partyName');
 
             if (!partyName || partyName.length < 3) {
                 this.set('error', 'Please enter a descriptive name');
@@ -31,9 +26,7 @@ export default Ember.ArrayController.extend(LoggedInMixin, {
                 slug: partyName.toLowerCase().replace(/ /g, '-')
             });
 
-            party.save().then(function () {
-                self.get('content').update();
-            });
+            party.save();
         }
     }
 });
